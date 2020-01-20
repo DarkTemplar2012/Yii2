654deeb7a1ddcbbc68fb38df70758279c8ae5222
Denis <denoms@bk.ru>
2020-01-21 01:28:29 +0600

    hw 2

--- models/User.php
+++ models/User.php
@@ -2,89 +2,128 @@
 
 namespace app\models;
 
+use app\models\tables\Users;
+
 class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
+  public $id;
+  public $username;
+  public $password;
+  public $authKey;
+  public $accessToken;
 
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
-
+//  private static $users = [
+//    '100' => [
+//      'id' => '100',
+//      'username' => 'admin',
+//      'password' => 'admin',
+//      'authKey' => 'test100key',
+//      'accessToken' => '100-token',
+//    ],
+//    '101' => [
+//      'id' => '101',
+//      'username' => 'demo',
+//      'password' => 'demo',
+//      'authKey' => 'test101key',
+//      'accessToken' => '101-token',
+//    ],
+//  ];
 
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function findIdentity($id)
-    {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+  /**
+   * {@inheritdoc}
+   */
+  public static function findIdentity($id)
+  {
+    $usr = Users::findOne($id);
+    if ($usr) {
+      return new static([
+        'id' => $usr->id,
+        'username' => $usr->name,
+        'password' => $usr->password,
+      ]);
+    }
+    return null;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function findIdentityByAccessToken($token, $type = null)
+  {
+    foreach (self::$users as $user) {
+      if ($user['accessToken'] === $token) {
+        return new static($user);
+      }
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function findIdentityByAccessToken($token, $type = null)
-    {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
+    return null;
+  }
 
+  /**
+   * Finds user by username
+   *
+   * @param string $username
+   * @return static|null
+   */
+  public static function findByUsername($username)
+  {
 
-        return null;
+//    self::$username = $result->name;
+    $usr = Users::findOne(['login' => $username]);
+    if ($usr) {
+      return new static([
+        'id' => $usr->id,
+        'username' => $usr->name,
+        'password' => $usr->password,
+      ]);
     }
+    return null;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getAuthKey()
-    {
-        return $this->authKey;
-    }
+//    var_dump(self::$users);
 
-    /**
-     * {@inheritdoc}
-     */
-    public function validateAuthKey($authKey)
-    {
-        return $this->authKey === $authKey;
-    }
+//    var_dump($result);
+//    foreach (self::$users as $user) {
+//      if (strcasecmp($user['username'], $username) === 0) {
+//        return new static($user);
+//      }
+//    }
+//
+  }
 
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
-    public function validatePassword($password)
-    {
-        return $this->password === $password;
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getAuthKey()
+  {
+    return $this->authKey;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateAuthKey($authKey)
+  {
+    return $this->authKey === $authKey;
+  }
+
+  /**
+   * Validates password
+   *
+   * @param string $password password to validate
+   * @return bool if password provided is valid for current user
+   */
+  public function validatePassword($password)
+  {
+    return $this->password === $password;
+  }
 }
